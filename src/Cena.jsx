03ad2eee9c2b33bdf8ffd9d041bd9a8ebd9673c9
Cristian <sonlatin2@gmail.com>
2023-04-20 19:32:06 -0500

    completed task

--- src/Cena.jsx
+++ src/Cena.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 
 function Cena({nombre, color, precio, setTotal }) {
+    const ruta= "https://www.html6.es/img/rey_"
+    const imagen = `${ruta}${nombre.toLowerCase()}.png`
+    const comprar = (element)=>{
+            setTotal((e)=>e+precio)
 
-    const comprar = ()=>{
-
+            element.target.parentNode.parentNode.style.display = "none"
     }
     return <>
-        <div className="rey">
+        <div className="rey" style={{backgroundColor: color}}>
             <h1>{nombre}</h1>
+                <img src={imagen} />
             <div className="titulo"> Precio:</div>
             <div className="precio">
                 {precio}
