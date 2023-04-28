3cfebdb1a86f3a89c48568282b2ee93ff88c0a81
Cristian <sonlatin2@gmail.com>
2023-04-27 19:59:03 -0500

    ejercicios completos

---
+++ Ej5/Actividad3.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Actividad3({reyes}) {
+    const resultado = reyes.find(e => e.nombre.substring(0,1) == "A" ||
+    e.nombre.substring(0,1) == "a")
+    console.log(resultado)
+    return <>
+        {resultado == undefined ? "No se ha encontrado": resultado.nombre}
+    </>
+}
+export default Actividad3
